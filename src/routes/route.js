3a5585a8ec8f6b7c60a01411154af99952e9131c
MohitRaykwar7389966852 <mohitraykwar99@gmail.com>
2022-04-06 17:11:15 +0530

    changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const xmod = require('../xmod');
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    res.send(xmod.add())
 });
-
 module.exports = router;
-// adding this comment for no reason
---
+++ src/xmod.js
@@ -0,0 +1,11 @@
+
+const print = "mohit raykwar"
+const url ="http://www.google.com"
+const address = function()
+{
+    return "gandhisagar no.3 , mandsaur , m.p."
+}
+// console.log(module)
+module.exports.print = print
+module.exports.url = url
+module.exports.add = address
