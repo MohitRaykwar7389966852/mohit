952a5c68d5a03f0e579a4e5cfb9b550d58ff9735
MohitRaykwar7389966852 <mohitraykwar99@gmail.com>
2022-04-06 17:18:41 +0530

    remove changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const xmod = require('../xmod');
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send(xmod.add())
+    res.send("this is my first ever api...")
 });
 module.exports = router;
