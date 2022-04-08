7b1e49f64f9e206abf9c9382d4411e3554a7e34d
MohitRaykwar7389966852 <mohitraykwar99@gmail.com>
2022-04-08 17:25:10 +0530

    thtrth

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,10 +6,10 @@ const router = express.Router();
 let movies = ['martian','avangers','batman','bahubali','pushpa','incidious','3idiots']
 
 //problem 1
-// router.get('/movies', function (req, res)
-// {
-//     res.send(movies)
-// });
+router.get('/movies', function (req, res)
+{
+    res.send(movies)
+});
 
 //problem 2 & 3
 // router.get('/movies', function (req, res)
@@ -50,18 +50,19 @@ const films = [
 // });
 
 //problem 5
-router.get('/films', function (req, res){
-    let id = req.query.filmid
-    let filmobject = films.find(x=> x.id == id)
-    if(filmobject == undefined)
-    {
-        res.send("No movie exists with this id")
-    }
-    else
-    {
-        res.send(filmobject)
-    }
-});
+// router.get('/films', function (req, res){
+//     let id = req.query.filmid
+//     let filmobject = films.find(x=> x.id == id)
+//     if(filmobject == undefined)
+//     {
+//         res.send("No movie exists with this id")
+//     }
+//     else
+//     {
+//         res.send(filmobject)
+//     }
+// });
 
 module.exports = router;
+
 // adding this comment for no reason
