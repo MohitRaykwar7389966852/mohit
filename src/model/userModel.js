7c41eb7cf73a1bbcb2536a27db8aaa29c8b1dba2
MohitRaykwar7389966852 <mohitraykwar99@gmail.com>
2022-04-11 23:27:54 +0530

    book database

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -3,21 +3,12 @@ const mongoose = require('mongoose');
 
 //we can create schema by mongoose to struct the data
 
-const userSchema = new mongoose.Schema({
-    firstName:String,
-    lastName:String,
-    number:{
-        type:String,
-        unique:true,
-    },
-    age:Number,
-    gender:{
-        type:String,
-        enum:["male","female"]
-    },
-    isIndian:Boolean,
-    hobby:[String],
+const bookSchema = new mongoose.Schema({
+    bookName:String,
+    authorName:String,
+    category:String,
+    year:Number
 },{timestamps:true});
 
-module.exports = mongoose.model('User' , userSchema) //this is a syntax to export the model
+module.exports = mongoose.model('User' , bookSchema) //this is a syntax to export the model
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const logger = require('./logger')
 const mongoose = require('mongoose');
-const userModel = require('../model/userModel');
+const bookModel = require('../model/userModel');
 const router = express.Router();
 
 
-mongoose.connect('mongodb+srv://mraykwar99:LvIVaS9x3LyxfoQV@cluster0.1d2my.mongodb.net/myDataBase?retryWrites=true&w=majority',{useNewurlParser:true})
+mongoose.connect('mongodb+srv://mraykwar99:LvIVaS9x3LyxfoQV@cluster0.1d2my.mongodb.net/bookDB?retryWrites=true&w=majority',{useNewurlParser:true})
 .then( () => console.log("mongoDb is connected"))
 .catch(err => console.log(err));
 
-router.get('/addUser', async function(req,res)
+router.get('/addBook', async function(req,res)
 {
     let data = req.body;
-    let dataAdded = await userModel.create(data)
+    let dataAdded = await bookModel.create(data)
     res.send(dataAdded)
+});
+
+router.get('/showBook', async function(req,res)
+{
+    let books = await bookModel.find()
+    res.send(books)
 });
 
 module.exports = router;
