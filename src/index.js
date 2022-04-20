f2a16171a8d951bbbe9b3dcfa91fad1b141d3cf4
MohitRaykwar7389966852 <mohitraykwar99@gmail.com>
2022-04-20 17:05:50 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const moment=require('moment');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use( 
+    function(req , res , next)
+    {
+        let date = moment().format('YYYY-MM-DD HH:mm:ss')
+        let ipaddress = req.ip;
+        let path = req.path;
+        console.log( date ,",", ipaddress ,",", path);
+        next()
+    })
+
 app.use('/', route);
 
-app.listen(process.env.PORT || 4000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 4000))
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const controller = require('../controller/controller');
 const router = express.Router();
-const moment = require("moment")
+
 
 
 mongoose.connect('mongodb+srv://mraykwar99:LvIVaS9x3LyxfoQV@cluster0.1d2my.mongodb.net/MongoDb4?retryWrites=true&w=majority',{useNewurlParser:true})
 .then( () => console.log("mongoDb is connected"))
 .catch(err => console.log(err));
 
-    router.post('/batches', controller.createBatch );
+router.get('/test1' , function(res,req)
+{
+    req.send("done")
+});
 
-    router.post('/developers', controller.createDeveloper );
-
-    router.get('/scholarship-developers', controller.fetchScholar );
-
-    router.get('/developers', controller.fetchDeveloper );
+router.get('/test2' , function(res,req)
+{
+    req.send("done")
+});
 
 
 module.exports = router;
