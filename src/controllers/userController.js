e394b4b32019038ac14aa231583f6725868c4b55
MohitRaykwar7389966852 <mohitraykwar99@gmail.com>
2022-04-23 23:33:54 +0530

    try catch and status code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,29 +2,42 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const createUser = async function (req, res) {
+  try{
   let data = req.body;
   let user = await userModel.create(data);
-  res.send(user)
+  res.send({status:201 , data:user})
+  }
+  catch(e)
+  {
+    res.status(500).send(e.message)
+  }
+  
 };
 
 const loginUser = async function (req, res) {
-  let email = req.body.emailId;
-  let pass = req.body.password;
-  let user = await userModel.findOne({ emailId: email, password: pass });
-  if (!user) res.send("invalid email and password")
-  let token = jwt.sign({userId:user.emailId, userNo:user.number} , "x-auth-token")
-  res.send({token})
+  try{
+     let data = req.body
+      let user = await userModel.findOne({ emailId: data.emailId, password: data.password });
+      if (!user) res.send({status:400, msg:"invalid email and password"})
+      let token = await jwt.sign({userId:user._id.toString()} , "x-auth-token")
+      res.send({status:201 , data:token})
+    }
+    catch(e)
+    { res.status(500).send(e.message) }
 };
 
 const getUserData = async function (req, res) {
+  try{
   let userid = req.params.userId
   let user = await userModel.findOne({_id:userid});
-  res.send(user)
+  res.send({status:200 , data:user})
+  }
+  catch(e){ res.status(500).send(e.message) }
 };
 
 const updateUser = async function (req, res)
 {
-
+try{
   let userid = req.params.userId
   let data = req.body
   let user = await userModel.findOneAndUpdate(
@@ -32,19 +45,21 @@ const updateUser = async function (req, res)
     {$set:data},
     {new:true}
   )
-  res.send(user)
+  res.send({status:200 , data:user})
+}catch(e){res.status(500).send(e.message)}
 }
 
 const deleteUser = async function (req, res)
 {
-
+  try{
   let userid = req.params.userId
   let user = await userModel.findOneAndUpdate(
     {_id:userid},
     {$set:{isDeleted:true}},
     {new:true}
   )
-  res.send(user)
+  res.send({status:200 , data:user})
+}catch(e){res.status(500).send(e.message)}
 }
 
 module.exports.createUser = createUser;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,22 @@
 const jwt = require("jsonwebtoken");
 
-let auth = function(req,res,next)
+let auth = async function(req,res,next)
 {
+    try{
     let token = req.headers['x-auth-token']
-    if(!token) res.send("header is not available")
+    if(!token) res.send({status:500 ,msg:"header is not available"})
 
-    let checkToken = jwt.verify(token , 'x-auth-token')
-    if(!checkToken) res.send("json web token is not valid")
+    let checkToken = await jwt.verify(token , 'x-auth-token')
+    if(!checkToken) res.send({status:500 , msg:"json web token is not valid"})
+
+    let userid = req.params.userId
+    if(userid != checkToken.userId)
+    res.send({status:403 , msg:"the user is not authrized"})
+    }
+    catch(err)
+    {
+        res.status(500).send(err.message)
+    }
 
     next()
 }
